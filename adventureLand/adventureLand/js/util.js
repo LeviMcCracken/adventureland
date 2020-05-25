3e0b33c0c4310ab5a18c98dea318b8f52ef644ad
LeviMcCracken <levi.mccracken@gmail.com>
2020-05-24 19:27:55 -0600

    Fixed unmatched brackets

--- adventureLand/adventureLand/js/util.js
+++ adventureLand/adventureLand/js/util.js
@@ -20,10 +20,9 @@ function setBuying() {
 function buy_potions(list) {
     let ret = false;
     for (item in list){
-            if (item_quantity(item) < buy_potions_up_to) {
-                buy(item, pots_at_a_time);
-                ret = true;
-            }
+        if (item_quantity(item) < buy_potions_up_to) {
+            buy(item, pots_at_a_time);
+            ret = true;
         }
     }
     if (character.gold <= gold_min_thresh) {
