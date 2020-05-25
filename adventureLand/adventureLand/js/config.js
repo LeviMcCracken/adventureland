fb8670c5d1547116b25d590f768d6afd54711535
LeviMcCracken <levi.mccracken@gmail.com>
2020-05-25 11:20:24 -0600

    Upped min gold to buy pots

--- adventureLand/adventureLand/js/config.js
+++ adventureLand/adventureLand/js/config.js
@@ -1,6 +1,6 @@
 var max_att_p = 1000;
 var buy_potions_up_to = 1000;
-var gold_min_thresh = 5000;
+var gold_min_thresh = 10000;
 var pots_at_a_time = 100;
 var min_pot_thresh = 20;
 var partyheal_thresh = 400;
